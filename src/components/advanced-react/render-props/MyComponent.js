d6f0e19753214b224609d7540213d59b8775d2c4
evondev <trananhtuan400@gmail.com>
2022-03-21 08:04:06 +0700

    recap render props

---
+++ src/components/advanced-react/render-props/MyComponent.js
@@ -0,0 +1,27 @@
+// import React from 'react';
+
+// const MyComponent = () => {
+//   return (
+//     <div>
+//       <ComponentA>
+//         {value => (
+//           <div>
+//             <ComponentB>
+//               {value2 => (
+//                 <div>
+//                   <ComponentC>
+//                     {value3  => (
+
+//                     )}
+//                   </ComponentC>
+//                 </div>
+//               )}
+//             </ComponentB>
+//           </div>
+//         )}
+//       </ComponentA>
+//     </div>
+//   );
+// };
+
+// export default MyComponent;
